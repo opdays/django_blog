540d21f15da58aa3211f8adc009a1907cee7e6e2
Yang Yang <yyang_yang1@kingdee.com>
2017-05-07 08:27:44 +0800

    s

--- article/static/article/md.js
+++ article/static/article/md.js
@@ -4,12 +4,13 @@
 
 $(function () {
         //响应式markdown 图片
-        $('.article-detail img').addClass("img-responsive img-rounded");
+        $('.article-detail img').addClass("img-responsive img-rounded center-block");
 
         //响应式markdown表格
         $('.post-content table').addClass("table table-bordered table-hover table-striped");
 
         //a标签打开标签页
         $('.post-content a').attr("target","_blank");
+
 });
 
