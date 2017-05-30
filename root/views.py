d3829efd1b9eb503e7878d47dffd73f6f8077ebf
Yang Yang <yyang_yang1@kingdee.com>
2017-05-30 17:05:58 +0800

    改变样式

--- root/views.py
+++ root/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from article.models import Article
-from django.http import Http404,HttpResponseForbidden,HttpResponseRedirect
+from django.http import Http404, HttpResponseForbidden, HttpResponseRedirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
-#https://docs.djangoproject.com/en/dev/topics/auth/default/
+# https://docs.djangoproject.com/en/dev/topics/auth/default/
 
 def root_login(request):
     if request.method == 'GET':
-        return render(request,"login.html")
+        return render(request, "login.html")
 
     if request.method == 'POST':
         username = request.POST.get("username")
@@ -23,21 +25,30 @@ def root_login(request):
             else:
                 return HttpResponseForbidden()
         except:
-                return HttpResponseForbidden()
+            return HttpResponseForbidden()
+    return render(request, "login.html")
+
+
 def root_logout(request):
     logout(request)
     return HttpResponseRedirect("/article/page/")
+
+
 @login_required(login_url='/root/login')
 def root_index(request):
-    return render(request,"_root_base.html")
+    return render(request, "_root_base.html")
+
+
 @login_required(login_url='/root/login')
 def root_article_list(request):
     articles = Article.objects.all()
-    return render(request,"article_manager.html",context={"articles":articles})
+    return render(request, "article_manager.html", context={"articles": articles})
+
+
 @login_required(login_url='/root/login')
-def root_article_editor(request,id):
+def root_article_editor(request, id):
     try:
         article = Article.objects.get(id=id)
     except:
         raise Http404()
-    return render(request,"article_editor.html",context={"article":article})
+    return render(request, "article_editor.html", context={"article": article})
